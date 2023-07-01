a5bfeb502e94b75f7f0187e8e072abc160c1cc07
Clement-soulier <clement.soulier12@gmail.com>
2023-07-01 12:28:46 +0200

    Changed data structure of chapters.js, added exam to chapter and made CSS for mobile screen

--- app/(backArrowTopBar)/Lessons/chapter1/page.js
+++ app/(backArrowTopBar)/Lessons/chapter1/page.js
@@ -5,14 +5,27 @@ import Link from 'next/link';
 import "./../chapter.css"
 
 export default function Page(){
-    const list = chapter1.map(chapter =>
-        <li key={chapter.id} className="listItem">
+    const list = chapter1.map(chapter =>{
+        if(chapter.exam){
+            return(
+            <li key={chapter.id} className='listItem'>
+                <Link href={""}>
+                    <h1 className='chapterId'>exam {chapter.id - 100}</h1>
+                    <h2 className='chapterName'>{chapter.name}</h2>
+                    <div className='progression'>00%</div>
+                </Link>
+            </li>);
+        }
+        else{
+            return(<li key={chapter.id} className="listItem">
             <Link href={""}>
-                <h1 className="chapterId">lesson {chapter.id}</h1>
+                <h1 className="chapterId">lesson {chapter.id + 1}</h1>
                 <h2 className="chapterName">{chapter.name}</h2>
                 <div className="progression">00%</div>
             </Link>
-        </li>)
+        </li>);
+        }
+    })
 
     return(
         <>
--- app/(backArrowTopBar)/Lessons/chapters.js
+++ app/(backArrowTopBar)/Lessons/chapters.js
@@ -30,210 +30,372 @@ export const chapters =[{
 
 export const chapter1 =[{
     id:0,
+    exam: false,
     name:"Do3, Mi3"
 },{
     id:1,
+    exam: false,
     name:"Sol3"
 },{
     id:2,
+    exam: false,
     name: "Si3"
 },{
     id:3,
+    exam: false,
     name: "Re3"
 },{
     id:4,
+    exam: false,
     name:"Fa3"
 },{
     id:5,
+    exam: false,
     name:"La3"
+}, {
+    id:101,
+    exam:true,
+    name:"3rd octave"
 }]
 
 export const chapter2 = [{
     id:0,
-    name:"Do3#, Sol3#",
+    exam:false,
+    name:"Explanations on sharps and flats"
 },{
     id:1,
-    name:"Si3#"
+    exam:false,
+    name:"Do3#, Sol3#",
 },{
     id:2,
-    name:"Re3#"
+    exam:false,
+    name:"Si3#"
 },{
     id:3,
+    exam:false,
+    name:"Re3#"
+},{
+    id:4,
+    exam:false,
     name:"La3#"
+},{
+    id:101,
+    exam:true,
+    name:"All sharps and flats from 3rd octave"
+},{
+    id:102,
+    exam:true,
+    name:"All tones and semitones from 3rd octave"
 }]
 
 export const chapter3 = [{
     id:0,
+    exam:false,
     name:"Do2, Mi2"
 },{
     id:1,
+    exam:false,
     name:"Sol2"
 },{
    id:2,
+   exam:false,
    name:"Si2" 
 },{
     id:3,
+    exam:false,
     name:"Re2"
 },{
     id:4,
+    exam:false,
     name:"Fa2"
 },{
     id:5,
+    exam:false,
     name:"La2"
 },{
+    id:101,
+    exam:true,
+    name:"2nd octave"
+},{
+    id:102,
+    exam:true,
+    name:"2nd and 3rd octaves"
+},{
     id:6,
+    exam:false,
     name:"Do2#"
 },{
     id:7,
+    exam:false,
     name:"Si2#"
 },{
     id:8,
+    exam:false,
     name:"Re2#"
 },{
     id:9,
+    exam:false,
     name:"La2#"
+},{
+    id:103,
+    exam:true,
+    name:"All 2nd octave"
+},{
+    id:104,
+    exam:true,
+    name:"All 2nd and 3rd octaves"
 }]
 
 export const chapter4 = [{
     id:0,
+    exam:false,
     name:"Do4, Mi4"
 },{
     id:1,
+    exam:false,
     name:"Sol4"
 },{
     id:2,
+    exam:false,
     name:"Si4"
 },{
     id:3,
+    exam:false,
     name:"Re4"
 },{
     id:4,
+    exam:false,
     name:"Fa4"
 },{
     id:5,
+    exam:false,
     name:"La4"
 },{
+    id:101,
+    exam:true,
+    name:"4th octave"
+},{
+    id:102,
+    exam:true,
+    name:"2nd, 3rd and 4th octaves"
+},{
     id:6,
+    exam:false,
     name:"Do4# et Sol4#"
 },{
     id:7,
+    exam:false,
     name:"Si4#"
 },{
     id:8,
+    exam:false,
     name:"Re4#"
 },{
     id:9,
+    exam:false,
     name:"La4#"
+},{
+    id:103,
+    exam:true,
+    name:"All 4th octave"
+},{
+    id:104,
+    exam:true,
+    name:"All 2nd, 3rd and 4th octave"
 }]
 
 export const chapter5 = [{
     id:0,
-    name:"MDo3, MSol3"
+    exam:false,
+    name:"Explanation on perfects majors chords"
 },{
     id:1,
-    name:"MMi3"
+    exam:false,
+    name:"MDo3, MSol3"
 },{
     id:2,
-    name:"MSi3"
+    exam:false,
+    name:"MMi3"
 },{
     id:3,
-    name:"MRe3"
+    exam:false,
+    name:"MSi3"
 },{
     id:4,
-    name:"MFa3"
+    exam:false,
+    name:"MRe3"
 },{
     id:5,
-    name:"MLa3"
+    exam:false,
+    name:"MFa3"
 },{
     id:6,
-    name:"MDo2, MSol2"
+    exam:false,
+    name:"MLa3"
+},{
+    id:101,
+    exam:true,
+    name:"Perfect majors chords from 3rd octave"
 },{
     id:7,
-    name:"MMi2"
+    exam:false,
+    name:"MDo2, MSol2"
 },{
     id:8,
-    name:"MSi2"
+    exam:false,
+    name:"MMi2"
 },{
     id:9,
-    name:"MRe2"
+    exam:false,
+    name:"MSi2"
 },{
     id:10,
-    name:"MFa2"
+    exam:false,
+    name:"MRe2"
 },{
     id:11,
-    name:"MLa2"
+    exam:false,
+    name:"MFa2"
 },{
     id:12,
-    name:"MDo4, MSol4"
+    exam:false,
+    name:"MLa2"
+},{
+    id:102,
+    exam:true,
+    name:"Perfect majors chords from 2nd octave"
+},{
+    id:103,
+    exam:true,
+    name:"Perfect majors chords from 2nd and 3rd octaves"
 },{
     id:13,
-    name:"MMi4"
+    exam:false,
+    name:"MDo4, MSol4"
 },{
     id:14,
-    name:"MSi4"
+    exam:false,
+    name:"MMi4"
 },{
     id:15,
-    name:"MRe4"
+    exam:false,
+    name:"MSi4"
 },{
     id:16,
-    name:"MFa4"
+    exam:false,
+    name:"MRe4"
 },{
     id:17,
+    exam:false,
+    name:"MFa4"
+},{
+    id:18,
+    exam:false,
     name:"MLa4"
+},{
+    id:104,
+    exam:true,
+    name:"Perfect majors chords from 4th octave"
+},{
+    id:105,
+    exam:true,
+    name:"Perfect majors chords from 2nd, 3rd and 4th octaves"
 }]
 
 export const chapter6 = [{
     id:0,
-    name:"mDo3, mSol3"
+    exam:false,
+    name:"Explanation on perfect minor chords"
 },{
     id:1,
-    name:"mMi3"
+    exam:false,
+    name:"mDo3, mSol3"
 },{
     id:2,
-    name:"mSi3"
+    exam:false,
+    name:"mMi3"
 },{
     id:3,
-    name:"mRe3"
+    exam:false,
+    name:"mSi3"
 },{
     id:4,
-    name:"mFa3"
+    exam:false,
+    name:"mRe3"
 },{
     id:5,
-    name:"mLa3"
+    exam:false,
+    name:"mFa3"
 },{
     id:6,
-    name:"mDo2, mSol2"
+    exam:false,
+    name:"mLa3"
+},{
+    id:101,
+    exam:true,
+    name:"Perfect minor chords form 3rd octave"
 },{
     id:7,
-    name:"mMi2"
+    exam:false,
+    name:"mDo2, mSol2"
 },{
     id:8,
-    name:"mSi2"
+    exam:false,
+    name:"mMi2"
 },{
     id:9,
-    name:"mRe2"
+    exam:false,
+    name:"mSi2"
 },{
     id:10,
-    name:"mFa2"
+    exam:false,
+    name:"mRe2"
 },{
     id:11,
-    name:"mLa2"
+    exam:false,
+    name:"mFa2"
 },{
     id:12,
-    name:"mDo4, mSol4"
+    exam:false,
+    name:"mLa2"
+},{
+    id:102,
+    exam:true,
+    name:"Perfect minor chords from 2nd octave"
+},{
+    id:103,
+    exam:true,
+    name:"Perfect minor chords from 2nd and 3rd octaves"
 },{
     id:13,
-    name:"mMi4"
+    exam:false,
+    name:"mDo4, mSol4"
 },{
     id:14,
-    name:"mSi4"
+    exam:false,
+    name:"mMi4"
 },{
     id:15,
-    name:"mRe4"
+    exam:false,
+    name:"mSi4"
 },{
     id:16,
-    name:"mFa4"
+    exam:false,
+    name:"mRe4"
 },{
     id:17,
+    exam:false,
+    name:"mFa4"
+},{
+    id:18,
+    exam:false,
     name:"mLa4"
+},{
+    id:104,
+    exam:true,
+    name:"Perfect minor chords from 4th octave"
+},{
+    id:105,
+    exam:true,
+    name:"Perfect minor chords from 2nd, 3rd and 4th octaves"
 }]
